2e5f391fa4d2e9826a44a55bc1d2c05a633ca549
Costin Lupu <costin.lup@gmail.com>
2018-06-16 01:47:06 +0300

    scripts: extend cloning script for github support

--- scripts/unikraft-clone-repos.sh
+++ scripts/unikraft-clone-repos.sh
@@ -6,7 +6,10 @@ if [ "$#" -ne 1 ]; then
 fi
 
 DIR="$1"
-REPO="git://xenbits.xen.org/unikraft"
+XEN_REPO_ROOT="git://xenbits.xen.org/unikraft"
+GITHUB_REPO_ROOT="https://github.com/nets-cs-pub-ro"
+USE_GITHUB=1
+
 LIBS=(\
 	newlib
         lwip
@@ -18,17 +21,30 @@ APPS=(\
 entry_dir() { pushd $1 &>/dev/null; }
 exit_dir()  { popd &>/dev/null;     }
 
+get_repo_root() {
+	[ $USE_GITHUB -eq 1 ] \
+		&& echo $GITHUB_REPO_ROOT \
+		|| echo $XEN_REPO_ROOT
+}
+
+get_repo_lib() {
+	local libname="$1"
+	[ $USE_GITHUB -eq 1 ] \
+		&& echo "$GITHUB_REPO_ROOT/unikraft-lib-$libname.git" \
+		|| echo "$XEN_REPO_ROOT/libs/$libname.git"
+}
+
 mkdir -p $DIR
 entry_dir $DIR
 
 # Clone kernel
-[ ! -d  "$DIR/unikraft" ] && git clone "$REPO/unikraft.git"
+[ ! -d  "$DIR/unikraft" ] && git clone "$(get_repo_root)/unikraft.git"
 
 # Clone libs
 mkdir -p "libs"
 entry_dir "libs"
 for l in ${LIBS[@]}; do
-	[ ! -d  "$DIR/libs/$l" ] && git clone "$REPO/libs/$l.git"
+	[ ! -d  "$DIR/libs/$l" ] && git clone "$(get_repo_lib $l)" $l
 done
 exit_dir
 
@@ -36,7 +52,7 @@ exit_dir
 mkdir -p "apps"
 entry_dir "apps"
 for a in ${APPS[@]}; do
-	[ ! -d  "$DIR/apps/$a" ] && git clone "$REPO/apps/$a.git"
+	[ ! -d  "$DIR/apps/$a" ] && git clone "$XEN_REPO_ROOT/apps/$a.git"
 done
 exit_dir
 
